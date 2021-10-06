cc48820d1a87f4e6876ab24f79b7dabd596c55ef
sumana10 <sumanaonlyme@gmail.com>
2021-10-06 18:18:59 +0530

    initial commit

---
+++ src/Components/BuyPage.js
@@ -0,0 +1,81 @@
+import React, {useState, useEffect} from 'react'
+//to fetch api
+import Axios from "axios";
+//to get fake data
+import {random, commerce, datatype} from "faker"
+//to do the ui
+import {Container, Col, Row} from "reactstrap"
+import CartItem from './CartItem';
+
+
+//this is my key from pexel
+const apiKey = "563492ad6f917000010000014adcc8c45bfd446d8629a315d4a42db2";
+//url for access
+const url = "https://api.pexels.com/v1/search?query=laptop&per_page=6&page=1";
+//my jsonpage url
+const localurl = "http://myjson.dit.upm.es/api/bins/1lt3"
+
+//here is the component which is loaded with props
+const BuyPage = ({addInCart})=>{
+
+  const [product, setProduct] = useState([]);
+
+  // const fetchPhotos = async() =>{
+  //   const response = await Axios.get(url,{
+  //     headers:{
+  //       Authorization: apiKey
+  //     }
+  //   })
+  // }
+
+  //how to fetch through axios with headers and without headers
+  const fetchPhotos = async() =>{
+    //const {data} = await Axios.get(localurl,{})
+    const {data} = await Axios.get(url,{
+
+      headers:{
+        Authorization: apiKey
+      }
+
+    })
+  
+  //destructuring data
+  const {photos} = data;
+
+
+  const allProduct = photos.map(photo =>({
+    smallImage: photo.src.medium,
+    tinyImage: photo.src.tiny,
+    productName: random.word(),
+    productPrice: commerce.price(),
+    id: datatype.uuid()
+
+  }))
+  setProduct(allProduct)
+
+  }
+  //load before handed
+  useEffect(() => {
+   fetchPhotos()
+  }, [])
+
+  return (
+    <Container fluid>
+      <h1 className="text-success text-center">Buy Page</h1>
+      <Row>
+      {product.map(product =>(
+
+        <Col md={4} product={product.id}>
+          <CartItem product={product} addInCart={addInCart}/>
+          
+         
+        </Col>
+      ))}
+      </Row>
+    
+    </Container>
+  )
+
+}
+
+export default BuyPage;
---
+++ src/Components/Cart.js
@@ -0,0 +1,76 @@
+import React from 'react'
+
+import {
+  Container,
+  ListGroup,
+  ListGroupItem,
+  Button,
+  CardHeader,
+  CardBody,
+  Card,
+  CardFooter,
+  Col,
+  Row
+} from "reactstrap"
+
+export default function Cart({cartItem, removeItem, buyNow}) {
+
+  let amount = 0;
+  cartItem.forEach(item =>{
+    amount = parseFloat(amount) + parseFloat(item.productPrice)
+  })
+  return (
+    <Container>
+      <h1 className="text-success">Your Cart</h1>
+      <ListGroup>
+      {cartItem.map(item =>(
+        <ListGroupItem key={item.id}>
+
+          <Row>
+            <Col>
+            <img 
+            height={80}
+            src={item.tinyImage} />
+            
+            </Col>
+            <Col className="text-center">
+              <div className="text-primary">
+                {item.productName}
+              </div>
+            <span>price:-{item.productPrice}</span>
+            <Button color="danger" onClick={()=>removeItem()}>Remove</Button>
+            </Col>
+          
+          </Row>
+        
+        </ListGroupItem>
+        
+      ))}
+      </ListGroup>
+      {
+        cartItem.length > 0 ? (
+
+          <Card className="text-center">
+            <CardHeader>
+              Grand Total
+            </CardHeader>
+            <CardBody>
+              Your amount for {cartItem.length} is {amount}
+            
+            </CardBody>
+            <CardFooter>
+              <Button color="success" onClick={buyNow}>pay here</Button>
+            </CardFooter>
+          
+          </Card>
+        ):(
+
+          <h1 className="text-warning">
+          Cart is Empty
+          </h1>
+          
+        )
+      }
+    </Container>
+  )
+}
